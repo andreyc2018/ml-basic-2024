00cc585a869099d97749b34e6d6ba98bbf7e3f75
Andrey <34249298+andreyc2018@users.noreply.github.com>
2024-06-16 01:07:06 -0400

    Added a whole bunch of classes

--- hw_5/main.py
+++ hw_5/main.py
@@ -2,61 +2,102 @@
 
 import datetime
 
+from abc import ABC, abstractmethod
+
 FILEFORMAT = []
 
-class Metadata:
-    def __init__(self) -> None:
-        self.name = ''
+class Metadata(ABC):
+    def __init__(self, path:str) -> None:
+        self.path = path
         self.size = 0
         self.created = datetime.datetime
         self.owner_id = 0
-        self.format = ''
+        self.format = path.split('.')[-1]
+
 
 class AudioMetadata(Metadata):
-    def __init__(self) -> None:
-        super().__init__()
-
-class VideoMetadata(Metadata):
-    def __init__(self) -> None:
-        super().__init__()
-
-class ImageMetadata(Metadata):
-    def __init__(self) -> None:
-        super().__init__()
-
-class File:
-    def __init__(self, path) -> None:
-        self.path = path
-
-class MediaFile:
-    def __init__(self) -> None:
-        self.metadata = None
-
-class Directory(File):
-    def __init__(self, path) -> None:
+    def __init__(self, path:str) -> None:
         super().__init__(path)
 
+
+class VideoMetadata(Metadata):
+    def __init__(self, path:str) -> None:
+        super().__init__(path)
+
+
+class ImageMetadata(Metadata):
+    def __init__(self, path:str) -> None:
+        super().__init__(path)
+
+
+class MediaConverter(ABC):
+    @abstractmethod
+    def run(self, media):
+        pass
+
+
+class AudioConverter(MediaConverter):
+    pass
+
+
+class MediaFile(ABC):
+    def __init__(self, meta, path:str) -> None:
+        self.md = meta(path)
+        self.buffer = ''
+
+    @abstractmethod
+    def read(self) -> None:
+        pass
+
+    @abstractmethod
+    def file_format(self) -> None:
+        pass
+
+    @abstractmethod
+    def convert(self) -> None:
+        pass
+
+
 class AudioFile(MediaFile):
-    def __init__(self) -> None:
-        super().__init__()
-        self.metadata = AudioMetadata()
+    def __init__(self, path:str) -> None:
+        super().__init__(AudioMetadata, path)
+        print(f'Create AuioFile {self.md.path}')
+
+    def read(self) -> None:
+        print(f'Read file {self.md.path}')
+
+    def file_format(self) -> None:
+        print(f'File {self.md.path} format {self.md.format}')
+
+    def convert(self, fmt:str) -> None:
+        print(f'Convert file {self.md.path} from {self.md.format} to {fmt}')
+        print(self.md.path.split('.')[:-1])
+        converter = AudioConverter()
+        converter.run(self)
+
 
 class VideoFile(MediaFile):
-    def __init__(self) -> None:
-        super().__init__()
-        self.metadata = VideoMetadata()
+    def __init__(self, path:str) -> None:
+        super().__init__(VideoMetadata, path)
+
 
 class ImageFile(MediaFile):
-    def __init__(self) -> None:
-        super().__init__()
-        self.metadata = ImageMetadata()
+    def __init__(self, path:str) -> None:
+        super().__init__(ImageMetadata, path)
 
 
-def main():
+def audio():
     audio = AudioFile('./test_audio.mp3')
     try:
         audio.read()
-        audio.file_type()
-        audio.convert()
+        audio.file_format()
+        audio.convert('wav')
     except Exception as ex:
         print(f'Error {ex}')
+
+
+def main():
+    audio()
+
+if __name__ == '__main__':
+    main()
